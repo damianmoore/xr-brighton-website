0d98df10f528fe1bdae18012ae9bebe812bde2d9
Damian Moore <damian@epixstudios.co.uk>
2019-06-19 18:10:31 +0100

    Extended event slug length

--- project/models.py
+++ project/models.py
@@ -34,7 +34,7 @@ class Category(VersionedModel):
 
 class Event(VersionedModel):
     name        = models.CharField(max_length=100)
-    slug        = models.CharField(max_length=60, blank=True, null=True)
+    slug        = models.CharField(max_length=110, blank=True, null=True)
     start       = models.DateTimeField()
     finish      = models.DateTimeField(blank=True, null=True)
     location    = models.TextField(blank=True)
