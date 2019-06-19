08a10b607507a08090844665eb08dc02d05bf3f3
Damian Moore <damian@epixstudios.co.uk>
2019-06-19 21:48:32 +0100

    Text styling, auto-links in descriptions, link styling

--- project/views.py
+++ project/views.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 from django.shortcuts import render
 import markdown
@@ -8,7 +9,12 @@ from .models import Event
 
 def event_detail(request, slug):
     event = Event.objects.get(slug=slug)
+
+    regex = r'http[s]*:\/\/[\w\S]+[\w\/]+'
+    description = re.sub(regex, lambda url: '[{0}]({0})'.format(url.group()), event.description)
+    description = markdown.markdown(description)
+
     return render(request, 'event_detail.html', {
         'event': event,
-        'description': markdown.markdown(event.description),
+        'description': description,
     })
